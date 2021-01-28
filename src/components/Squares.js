dc06b52763918395f863e17e52bb0f2cacffc6a0
Kishkuman13 <amos.martineau@gmail.com>
2021-01-27 21:23:30 -0700

    fixed code

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -34,7 +34,7 @@ export default function Squares() {
     // as the argument matches the active square in state, empty string otherwise.
     // Right-click and "inspect element" on the square to see its effect.
     
-    return id === activeSquare ? 'active' : '';
+    return id === activeSquare ? ' active' : '';
   };
 
   const markActive = id => {
